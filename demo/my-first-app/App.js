47a8d4afdf30babaff7539dc4f0331d587d72b38
msnkr <mikyle.designs@gmail.com>
2024-12-12 09:11:32 +0200

    .

--- demo/my-first-app/App.js
+++ demo/my-first-app/App.js
@@ -1,26 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import "./global.css";
+import { Text, View } from "react-native-web";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image
-        style={{ width: "100%", height: "400px" }}
-        source={{
-          uri: "https://images.pexels.com/photos/15539380/pexels-photo-15539380/free-photo-of-a-great-white-shark-swimming-in-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        }}
-      />
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-2xl">Hello, NativeWind!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
