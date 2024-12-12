a0b0fff34ac1c18dc5a5d65c65d2bd6de42bdaf9
msnkr <mikyle.designs@gmail.com>
2024-12-12 10:46:54 +0200

    .

---
+++ demo-2/my-app/App.js
@@ -0,0 +1,12 @@
+import "./global.css";
+import { Text, View } from "react-native-web";
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-2xl">Hello, NativeWind!</Text>
+    </View>
+  );
+};
+
+export default App;
