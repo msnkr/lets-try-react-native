8f0f0d12bc7b5be1bf6dd70d7a684b43f6173cc9
msnkr <mikyle.designs@gmail.com>
2024-12-12 11:06:22 +0200

    .

--- demo-2/my-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
