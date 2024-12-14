8833374273543b7fb5a2b258b62bdccd79d6278e
msnkr <mikyle.designs@gmail.com>
2024-12-14 23:26:44 +0200

    .

--- demo/my-new-project/App.js
+++ demo/my-new-project/App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.dummyText}>Hello, World!</Text>
+      <View>
+        <Button onPress={() => alert("Hi")} title="Tap Me!" />
+      </View>
+      <View>
+        <Text style={styles.dummyText}>This is a test</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    padding: 12,
+    borderColor: "green",
+    borderWidth: 4,
+    marginTop: 6,
+    marginBottom: 6,
+    backgroundColor: "black",
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
