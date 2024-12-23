10f90335092d0423775ce245d0a892286b2a3640
msnkr <mikyle.designs@gmail.com>
2024-12-23 22:32:47 +0200

    .

--- my-app/serenity/components/RandomMessage.js
+++ my-app/serenity/components/RandomMessage.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native-web";
-import data from "../data/data.json";
 
 const RandomMessage = () => {
   return (
     <View>
-      <Text>
-        {data.messages[Math.floor(Math.random() * data.messages.length)]}
-      </Text>
+      <Text>fucking work</Text>
     </View>
   );
 };
