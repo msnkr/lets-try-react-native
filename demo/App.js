d0251a943b3395855841674b41c9df021f317140
msnkr <mikyle.designs@gmail.com>
2025-01-10 22:50:36 +0200

    .

--- demo/App.js
+++ demo/App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
+import { useState } from "react";
 import "@expo/metro-runtime";
 
 export default function App() {
+  const [todos, setTodos] = useState(["todo1", "todo2", "todo3"]);
+
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={{ width: "60%", paddingHorizontal: 12, borderWidth: 1 }}
+          placeholder="Add todo"
+        />
+        <Button title="Add" />
+      </View>
+      <ScrollView style={styles.goalsContainer}>
+        {todos.map((todo, index) => (
+          <Text key={index}>{todo}</Text>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +34,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  textInputContainer: {
+    flexDirection: "row",
     justifyContent: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+  },
+  goalsContainer: {
+    paddingTop: 20,
   },
 });
