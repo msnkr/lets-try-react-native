ef3c9b9f70a3a992be40ae101219487fd712191a
msnkr <mikyle.designs@gmail.com>
2025-01-10 12:06:39 +0200

    .

--- demo/my-new-project/App.js
+++ demo/my-new-project/App.js
@@ -1,41 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import "@expo/metro-runtime";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { registerRootComponent } from "expo";
 registerRootComponent(App);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.boxOne}>Box 1</Text>
-      <Text style={styles.boxTwo}>Box 2</Text>
-      <Text style={styles.boxThree}>Box 3</Text>
+    <View>
+      <View>
+        <TextInput placeholder="Enter your name" />
+        <Button title="Submit" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  boxOne: {
-    flex: 1,
-    backgroundColor: "violet",
-    padding: 20,
-  },
-  boxTwo: {
-    flex: 1,
-    backgroundColor: "gold",
-    padding: 20,
-  },
-  boxThree: {
-    flex: 1,
-    backgroundColor: "coral",
-    padding: 20,
-  },
-});
+const styles = StyleSheet.create({});
