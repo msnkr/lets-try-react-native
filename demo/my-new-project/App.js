4d134f69118cdfebfc2b4ab43ddc3aa57a00ed73
msnkr <mikyle.designs@gmail.com>
2025-01-10 13:21:30 +0200

    .

--- demo/my-new-project/App.js
+++ demo/my-new-project/App.js
@@ -1,15 +1,33 @@
 import "@expo/metro-runtime";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-import { registerRootComponent } from "expo";
-registerRootComponent(App);
+
+import { useState } from "react";
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState(["milk", "bread", "eggs"]);
+
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputElement} placeholder="Enter your name" />
+        <TextInput
+          style={styles.inputElement}
+          placeholder="Enter your name"
+          onChangeText={handleChange}
+        />
         <Button title="Submit" />
+      </View>
+      <View style={styles.itemContainer}>
+        {items.map((item, index) => (
+          <li key={index} style={styles.listItems}>
+            {item}
+          </li>
+        ))}
       </View>
       <StatusBar style="auto" />
     </View>
@@ -19,12 +37,12 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "center",
   },
   inputElement: {
     borderWidth: 2,
@@ -32,5 +50,17 @@ const styles = StyleSheet.create({
     height: 36,
     borderRadius: 5,
     padding: 5,
+  },
+  itemContainer: {
+    flex: 5,
+    alignItems: "center",
+  },
+  listItems: {
+    padding: 10,
+    margin: 5,
+    textTransform: "capitalize",
+    fontSize: 18,
+    fontWeight: "bold",
+    cursor: "pointer",
   },
 });
