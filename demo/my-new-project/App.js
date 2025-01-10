7208117d61ce52d66c7da02bce898f3ce4e621c9
msnkr <mikyle.designs@gmail.com>
2025-01-10 15:48:40 +0200

    .

--- demo/my-new-project/App.js
+++ demo/my-new-project/App.js
@@ -4,10 +4,28 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <Text style={styles.box}>Box 1</Text>
+        <Text style={styles.box}>Box 2</Text>
+        <Text style={styles.box}>Box 3</Text>
+      </View>
+      <View style={{ flex: 3 }}>
+        <Text style={styles.box}>Box 4</Text>
+        <Text style={styles.box}>Box 5</Text>
+        <Text style={styles.box}>Box 6</Text>
+        <Text style={styles.box}>Box 7</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#f0f",
+    padding: 20,
+    margin: 10,
+    textAlign: "center",
+  },
+});
