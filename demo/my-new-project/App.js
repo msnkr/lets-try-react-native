b30a5a8ebf7c20db262ff1f7970ea257fc6c0915
msnkr <mikyle.designs@gmail.com>
2025-01-10 11:51:00 +0200

    .

--- demo/my-new-project/App.js
+++ demo/my-new-project/App.js
@@ -7,7 +7,9 @@ registerRootComponent(App);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Hello, World!</Text>
+      <Text style={styles.boxOne}>Box 1</Text>
+      <Text style={styles.boxTwo}>Box 2</Text>
+      <Text style={styles.boxThree}>Box 3</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,12 +18,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  textStyle: {
-    fontSize: 20,
-    color: "red",
+  boxOne: {
+    flex: 1,
+    backgroundColor: "violet",
+    padding: 20,
+  },
+  boxTwo: {
+    flex: 1,
+    backgroundColor: "gold",
+    padding: 20,
+  },
+  boxThree: {
+    flex: 1,
+    backgroundColor: "coral",
+    padding: 20,
   },
 });
