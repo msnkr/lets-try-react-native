4f4ddc5b0f3f91f68fa3deaa5cb1013e48395def
msnkr <mikyle.designs@gmail.com>
2025-01-11 17:32:29 +0200

    Add random motivational quotes feature with background image

--- serenity/App.js
+++ serenity/App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import "@expo/metro-runtime";
 
+import image from "./assets/1.jpg";
+import data from "./data/motivationData.json";
+
 export default function App() {
+  const RandomQuote = (data) => {
+    const newData = data.data.quotes;
+    const randomIndex = Math.floor(Math.random() * newData.length);
+
+    return <Text style={{ color: "white" }}>{newData[randomIndex].quote}</Text>;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
+    <ImageBackground source={image} style={styles.image}>
+      <RandomQuote data={data} />
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  image: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    width: "100%",
+    height: "100%",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
