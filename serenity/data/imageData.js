10843b77e3fa68118f0c96fde32c0dfdc0798d1d
msnkr <mikyle.designs@gmail.com>
2025-01-13 15:13:06 +0200

    Add additional images to the image array for enhanced background options

--- serenity/data/imageData.js
+++ serenity/data/imageData.js
@@ -1,10 +1,16 @@
-import imageOne from "../assets/1-new.jpg";
-import imageTwo from "../assets/2-new.jpg";
-import imageThree from "../assets/3-new.jpg";
-import imageFour from "../assets/4-new.jpg";
-import imageFive from "../assets/5-new.jpg";
-import imageSix from "../assets/6-new.jpg";
-import imageSeven from "../assets/7-new.jpg";
+import imageOne from "../assets/1.jpg";
+import imageTwo from "../assets/2.jpg";
+import imageThree from "../assets/3.jpg";
+import imageFour from "../assets/4.jpg";
+import imageFive from "../assets/5.jpg";
+import imageSix from "../assets/6.jpg";
+import imageSeven from "../assets/7.jpg";
+import imageEight from "../assets/8.jpg";
+import imageNine from "../assets/9.jpg";
+import imageTen from "../assets/10.jpg";
+import imageEleven from "../assets/11.jpg";
+import imageTwelve from "../assets/12.jpg";
+import imageThirteen from "../assets/13.jpg";
 
 const imageArr = [
   imageOne,
@@ -14,6 +20,12 @@ const imageArr = [
   imageFive,
   imageSix,
   imageSeven,
+  imageEight,
+  imageNine,
+  imageTen,
+  imageEleven,
+  imageTwelve,
+  imageThirteen,
 ];
 
 export default imageArr;
