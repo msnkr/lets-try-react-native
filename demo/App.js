f796f97908c46066645da1279d999b7eabf3cc9e
msnkr <mikyle.designs@gmail.com>
2025-01-13 09:31:19 +0200

    Add bottom border and padding to input container for improved layout

--- demo/App.js
+++ demo/App.js
@@ -33,13 +33,19 @@ export default function App() {
         />
         <Button title="Add" onPress={handlePress} />
       </View>
-      <ScrollView>
-        <FlatList
-          data={todos}
-          renderItem={(todo) => {
-            return <Text>{todo.item}</Text>;
-          }}
-        />
+      <ScrollView
+        style={styles.todoContainer}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View>
+          <FlatList
+            style={styles.flatListContainer}
+            data={todos}
+            renderItem={(todo) => {
+              return <Text style={styles.todoItem}>{todo.item}</Text>;
+            }}
+          />
+        </View>
       </ScrollView>
       <StatusBar style="auto" />
     </View>
@@ -54,10 +60,26 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: "row",
     justifyContent: "center",
+    borderBottomWidth: 2,
+    paddingBottom: 20,
   },
   inputText: {
     width: "60%",
     paddingHorizontal: 12,
     borderWidth: 2,
+  },
+  todoContainer: {
+    flex: 5,
+    paddingTop: 40,
+  },
+  todoItem: {
+    backgroundColor: "black",
+    color: "white",
+    paddingLeft: 80,
+    fontSize: 22,
+    textTransform: "capitalize",
+    margin: 4,
+    borderRadius: 8,
+    padding: 4,
   },
 });
