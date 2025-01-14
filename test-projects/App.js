e7586e612523713ed25318435323859d5a1aa93a
msnkr <mikyle.designs@gmail.com>
2025-01-14 16:09:15 +0200

    Remove unused FlatList and ScrollView components from App.js

--- test-projects/App.js
+++ test-projects/App.js
@@ -2,22 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
 import "@expo/metro-runtime";
 
-import data from "./data/fitnessData.json";
-
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
         <Text>Welcome to App</Text>
       </View>
-      <ScrollView showsHorizontalScrollIndicator={false}>
-        <FlatList
-          data={data}
-          renderItem={(item) => {
-            return;
-          }}
-        />
-      </ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
