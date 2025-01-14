70d1f4acb09f6168bcae8dcec1acee985a88611f
msnkr <mikyle.designs@gmail.com>
2025-01-14 15:35:38 +0200

    Add ScrollView to App component and integrate FlatList for better data display

--- test-projects/App.js
+++ test-projects/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
 import "@expo/metro-runtime";
 
 import data from "./data/fitnessData.json";
@@ -8,8 +8,20 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.headerText}>Welcome to app</Text>
+        <Text>Welcome to App</Text>
       </View>
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <FlatList
+          data={data}
+          renderItem={(item) => {
+            return (
+              <Text>
+                {item.item.name}: {item.item.primaryMuscles}
+              </Text>
+            );
+          }}
+        />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,12 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 100,
+    backgroundColor: "#fff",
     alignItems: "center",
-  },
-  headerText: {
-    fontSize: 32,
-    fontWeight: "bold",
-    textTransform: "uppercase",
+    justifyContent: "center",
   },
 });
