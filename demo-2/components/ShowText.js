653838292b88161609ad0c6dda2a8b9917254aa8
msnkr <mikyle.designs@gmail.com>
2025-01-21 11:29:50 +0200

    Enhance ShowText component by adding styling for improved text presentation

--- demo-2/components/ShowText.js
+++ demo-2/components/ShowText.js
@@ -3,7 +3,13 @@ import { StyleSheet, Text } from "react-native";
 import "@expo/metro-runtime";
 
 const ShowText = () => {
-  return <Text>Hello, World!</Text>;
+  return <Text style={styles.textComponent}>Hello, World!</Text>;
 };
 
 export default ShowText;
+
+const styles = StyleSheet.create({
+  textComponent: {
+    fontSize: 22,
+  },
+});
