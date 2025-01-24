ac503c18f2e9220352396da97541681b4961f0d8
msnkr <mikyle.designs@gmail.com>
2025-01-24 14:03:10 +0200

    Remove SearchComponent and update App.js to use ReturnExerciseComponent

--- gym-project/App.js
+++ gym-project/App.js
@@ -1,16 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList, Image } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import "@expo/metro-runtime";
-
-import data from "./data.json";
-const imagePath = "./assets/exercises/";
-
-import SearchComponent from "./components/SearchComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SearchComponent />
+      <ReturnExerciseComponent />
       <StatusBar style="auto" />
     </View>
   );
