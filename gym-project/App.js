b008d63f821dc19bd7a07da0618b952be630e567
msnkr <mikyle.designs@gmail.com>
2025-01-24 14:17:28 +0200

    Implement FlatList to display random exercises and update App.js layout

--- gym-project/App.js
+++ gym-project/App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, Pressable } from "react-native";
 import "@expo/metro-runtime";
+
+// Get an arr of random items for the homepage
+// display them
+// when clicked on, display information
+// Create search bar
+// Search items must display on page
+
+import data from "./data.json";
+
+const exerciseArr = [];
+
+for (let index = 0; index < 10; index++) {
+  exerciseArr.push(data[Math.floor(Math.random() * 100)]);
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ReturnExerciseComponent />
+      <FlatList
+        data={exerciseArr}
+        renderItem={(item) => {
+          const lowerCase = item.item.name.toLowerCase();
+          console.log(lowerCase.replace(/ /g, "_"));
+          return (
+            <View>
+              <Text>{item.item.name}</Text>
+              <Text>{item.item.level}</Text>
+              <Text>{item.item.primaryMuscles}</Text>
+            </View>
+          );
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
